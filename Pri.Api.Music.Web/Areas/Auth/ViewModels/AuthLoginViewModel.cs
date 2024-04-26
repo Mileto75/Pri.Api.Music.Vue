838dccafac87c1dd53baf8f13d32be5a7aa512e4
Mileto Di Marco <mileto.di.marco@howest.be>
2024-04-26 14:44:30 +0200

    Add project files.

---
+++ Pri.Api.Music.Web/Areas/Auth/ViewModels/AuthLoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Pri.Api.Music.Web.Areas.Auth.ViewModels
+{
+    public class AuthLoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Username { get; set; }
+        [DataType(DataType.Password)]
+        [Required]
+        public string Password { get; set; }
+        [HiddenInput]
+        public string ReturnUrl { get; set; }
+    }
+}
